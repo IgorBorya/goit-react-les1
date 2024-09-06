f0c2171ad66fba139e43b8ed68e61acb180fce50
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-09-06 16:28:00 +0300

    create first components header and footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <span>&Copy; All right reserved! 2024</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -0,0 +1,37 @@
+const WelcomeSection = () => {
+  return (
+    <div>
+      <section>
+        <h2>Welcome to React!</h2>
+        <ul>
+          <li>Hello</li>
+        </ul>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
+          similique? Veniam quae aspernatur reiciendis ullam impedit hic
+          repellat. Esse, commodi voluptatibus. Numquam obcaecati esse ut
+          nostrum iure nemo vero commodi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, eveniet!
+          Alias vitae quae obcaecati dolores neque accusamus totam voluptates
+          vel molestiae quaerat. Officiis quia at ipsa corporis nulla culpa
+          doloribus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          blanditiis quaerat earum ab modi illum voluptas perferendis optio
+          assumenda accusamus, commodi provident! Suscipit facere possimus
+          corrupti voluptate maxime et distinctio!
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default WelcomeSection;
