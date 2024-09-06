17250ffb31033a0fa3d1a4b68b703d70cdaa24be
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-09-06 17:15:34 +0300

    add full lesson 1 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Message from "./components/Message/Message";
 
 const App = () => {
+  const isOnline = false;
+  const isLoading = false;
+  const age = 13;
+  const filmsData = [
+    { id: "1", title: "Got" },
+    { id: "2", title: "Taxi" },
+    { id: "3", title: "Terminator" },
+  ];
+  const goodsData = [
+    { id: "1", title: "Apple" },
+    { id: "2", title: "Onion" },
+    { id: "3", title: "milk" },
+  ];
   return (
-    <div>
+    <>
       <Header />
-
+      <p>Супер Форум</p>
+      {isOnline && <h2>Welcome</h2>}
+      {isLoading && <h2>Loading</h2>}
+      {age > 18 ? <h2>You are so adult</h2> : <h2>You are too little</h2>}
+      <Message author="Petro" message="Sell Sofa" />
+      <Message message="Hello" />
+      <List title="films" data={filmsData} />
+      <List title="Goods" data={goodsData} />
       <Footer />
-    </div>
+    </>
   );
 };
 export default App;
